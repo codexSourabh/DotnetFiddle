aae8481c4c6964d846555c816f174ecd0d30eb65
codexSourabh <sourabhlokhande5@gmail.com>
2022-03-26 01:42:44 +0530

    adding fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+	static int fibo(int n)
+	{
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		else
+			return (fibo(n - 1) + fibo(n - 2));
+	}
+
+	public static void Main()
+	{
+		int num = 10;
+
+		for (int i = 0; i < num; i++)
+		{
+			Console.WriteLine(fibo(i));
+		}
+	}
+}
