99f089f67cc4a527cf71f5a654dbe54da7b0b4f9
codexSourabh <sourabhlokhande5@gmail.com>
2022-03-28 23:43:44 +0530

    adding vowel count app

---
+++ VowelCount/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		string str = "Hello";
+
+		var vowel = str.Where(x => x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u');
+
+		Console.WriteLine("Vowels are:{0}", vowel.Count());
+	}
+}
